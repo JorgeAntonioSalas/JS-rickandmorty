562d026c6b51d3ef936479a4537ef80a1773932f
bri <jorge.salas.sheron@gmail.com>
2023-08-29 23:12:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+import Location from "./Components/Location";
+
+function App(){
+
+return (
+        <div className='App'>
+          <div className='banner'>
+          </div>
+          <Location/>
+        </div>
+    );
+}
+
+
+ export default App;
